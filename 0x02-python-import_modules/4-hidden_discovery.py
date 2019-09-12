866902a7aa98271d52401fb608b48f5b60c9b86e
Sasha <alex.mapatelian@gmail.com>
2019-09-12 21:10:32 +0000

    pep8 validation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, hidden_4
+    import sys
+    import hidden_4
     for i in dir(hidden_4):
-        if i [0] is not '_':
+        if i[0] is not '_':
             print(i)
