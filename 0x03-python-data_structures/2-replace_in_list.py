49f1154da7a7f02ce9653fdc32741bc20f73982e
Sasha <alex.mapatelian@gmail.com>
2019-09-18 07:48:35 +0000

    fixed if statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if my_list:
-        if idx > len(my_list) or idx < 0:
-            return None
+        if idx >= len(my_list) or idx < 0:
+            return my_list
         my_list[idx] = element
         return my_list
