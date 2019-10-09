9ed987132e9252c2eee754ac91678c0640d2783d
Sasha <alex.mapatelian@gmail.com>
2019-10-09 20:41:33 +0000

    added a for loop

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -9,5 +9,18 @@ def only_diff_elements(set_1, set_2):
             newly created set
     """
     new = []
-    new = list(set_1) + list(set_2)
-    return set(new)
+    for i in set_1:
+        for k in set_2:
+            if i is k:
+                new.append(i)
+
+    new1 = list(set_1) + list(set_2)
+
+    new2 = []
+
+    for j in new1:
+        for u in new:
+            if j is not u:
+                new2.append(j)
+
+    return new2
