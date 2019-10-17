03ac998e96c746c07f8c25e83b1d8ae8a98a6623
Sasha <alex.mapatelian@gmail.com>
2019-10-17 06:48:11 +0000

    function that writes a string to a text file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/urs/bin/python3
+def write_file(filename="", text=""):
+    """Writes a string to a text file
+        Args:
+            filename (str): name of the file
+            text (str): string to be input
+    """
+    with open(filename, 'w', encoding='utf-8') as filie:
+        characters = filie.write(text)
+    return characters
