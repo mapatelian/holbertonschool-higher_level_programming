953f70f5dfbd8cea10a581f01b3304d096be1189
Sasha <alex.mapatelian@gmail.com>
2019-10-18 02:53:08 +0000

    class BaseGeometry with a public instance added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that initializes a new class
+"""
+
+
+class BaseGeometry:
+    """New class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
