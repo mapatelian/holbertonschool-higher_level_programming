76cd0276611754af794d4ebd37fb1621373579b6
Sasha <alex.mapatelian@gmail.com>
2019-10-18 03:12:36 +0000

    class BaseGeometry with a new method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that initializes a new class
+"""
+
+
+class BaseGeometry:
+    """New class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if isinstance(value, int) is False:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
