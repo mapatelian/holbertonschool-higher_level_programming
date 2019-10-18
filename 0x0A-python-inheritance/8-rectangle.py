14ac835811fa7c1f7176e5e0063241b21bcea7c3
Sasha <alex.mapatelian@gmail.com>
2019-10-18 05:37:25 +0000

    added calls to super()

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,5 +10,7 @@ class Rectangle(BaseGeometry):
     """Class Rectangle that inherits from BaseGeometry
     """
     def __init__(self, width, height):
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
         self.__height = height
         self.__width = width
