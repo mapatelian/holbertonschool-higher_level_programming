c6288201ae6ae87d8c1488f11276b9f0352f4a09
Sasha <alex.mapatelian@gmail.com>
2019-10-21 10:46:52 +0000

    pep8 validation + docstrings

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""Module that initializes a new class
+"""
 
 from models.base import Base
 
+
 class Rectangle(Base):
+    """Class Rectangle that inherits from class Base
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -13,10 +20,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Retriveing width
+        """
         return self.width
 
     @width.setter
     def width(self, value):
+        """Width validator and setter
+        """
         if isinstance(value, int) is not True:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -26,10 +37,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Retrieving height
+        """
         return self.height
 
     @height.setter
     def height(self, value):
+        """Height validator and setter
+        """
         if isinstance(value, int) is not True:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -39,10 +54,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Retrieving x
+        """
         return self.x
 
     @x.setter
     def x(self, value):
+        """X validator and setter
+        """
         if isinstance(value, int) is not True:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -52,10 +71,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Retrieving y
+        """
         return self.y
 
     @y.setter
     def y(self, value):
+        """Y validator and setter
+        """
         if isinstance(value, int) is not True:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -64,14 +87,19 @@ class Rectangle(Base):
             self.__y = value
 
     def area(self):
+        """Computing area
+        """
         return self.__width * self.__height
 
     def display(self):
+        """Displays area with the '#' key,
+            according to the x- and y-axis
+        """
         for k in range(self.__y):
             print()
         for i in range(self.__height):
             print(' ' * self.__x, end='')
-            for z in range (self.__width):
+            for z in range(self.__width):
                 print('#', end='')
             print()
 
@@ -79,9 +107,15 @@ class Rectangle(Base):
 # here in __str__ you might need to find a way how to print name of the class
 # Rectangle without hardcoding ot
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        """Overriding __str__ method
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".\
+            format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args):
+        """Updating class Rectangle: assignment an argument to each attribute
+            with the non-keyworded arguments
+        """
         lenght = len(args)
 
         if lenght == 1:
