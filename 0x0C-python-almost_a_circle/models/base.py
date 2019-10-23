3bd7d2d628a0de427b5c4329049d8062b8734cec
Sasha <alex.mapatelian@gmail.com>
2019-10-23 06:11:48 +0000

    fixed json method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,4 +27,4 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         else:
-            return str(list_dictionaries)
+            return json.dumps(list_dictionaries)
