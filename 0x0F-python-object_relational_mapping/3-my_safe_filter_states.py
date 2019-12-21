9c865b33b2d1843c2561083bd3147b569d9a2762
Sasha <alex.mapatelian@gmail.com>
2019-12-21 07:54:27 +0000

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,26 +7,19 @@ import MySQLdb
 import sys
 
 
-def list_all_match(mysql_username="", mysql_password="", database_name="",
-                   match=""):
-    """Takes an argument and displays all values in the states table
-    of hbtn_0e_0_usa where name matches the argument"""
-    con = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                          passwd=mysql_password, db=database_name,
-                          charset="utf8")
-    cur = con.cursor()
-    sql = "SELECT * FROM states WHERE name =%s ORDER BY id ASC"
-    cur.execute(sql, (match,))
-    query_rows = cur.fetchall()
-    for row in query_rows:
+if __name__ == '__main__':
+    username = sys.argv[1]
+    pswd = sys.argv[2]
+    dname = sys.argv[3]
+    state_serch = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=pswd, db=dname, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC;",
+                (state_serch,))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
-    con.close()
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    match = sys.argv[4]
-    list_all_match(username, password, database, match)
+    db.close()
