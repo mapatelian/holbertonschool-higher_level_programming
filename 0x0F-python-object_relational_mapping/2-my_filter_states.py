a7d46e37730ef3aa38a4a57aefe448152d02dfd3
Sasha <alex.mapatelian@gmail.com>
2019-12-21 07:53:26 +0000

    fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,28 +7,20 @@ import MySQLdb
 import sys
 
 
-def list_all_match(mysql_username="", mysql_password="", database_name="",
-                   match=""):
-    """
-    Displays all values in the states table that match the argument
-    """
-    con = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                          passwd=mysql_password, db=database_name,
-                          charset="utf8")
-    cur = con.cursor()
-    sql = "SELECT * FROM states WHERE name"
-    sql = sql + " LIKE BINARY '{}' ORDER by id ASC".format(match)
-    cur.execute(sql)
-    query_rows = cur.fetchall()
-    for row in query_rows:
+if __name__ == '__main__':
+    username = sys.argv[1]
+    pswd = sys.argv[2]
+    dname = sys.argv[3]
+    state_serch = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=pswd, db=dname, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY " +
+                "name='{}' ".format(state_serch) +
+                "ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
-    con.close()
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    match = sys.argv[4]
-    list_all_match(username, password, database, match)
+    db.close()
